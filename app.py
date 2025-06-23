3d302a9f3e88b599e2e59a06f847b500a58ad3a5
XLYing23333 <XLYing23333@gmail.com>
2025-06-23 09:01:15 +0800

    env: Create a new environment

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
 
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.title("Column 1")
-with col2:  
-    st.title("Column 2")
-with col3:
-    st.title("Column 3")
     
